8716f6be700e12a3def76b0a26c5a54af85b44b6
airlessproject <vlatko.mitreski@gmail.com>
2017-01-27 01:00:18 +0100

    sdclang
    Change-Id: I633e282c3135e4373a9ced9ee7499d28e418cf22

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,16 @@ DEVICE_PATH := device/sony/ivy
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
 
+ifneq ($(HOST_OS),darwin)
+
+SDCLANG := true
+
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
+
+SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+
+endif
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := E6533,E6553,ivy,ivy_dsds
 
